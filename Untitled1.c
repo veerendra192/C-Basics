7495197babf0e67becbbce026535b7758fe1389f
veerendra192 <86603708+veerendra192@users.noreply.github.com>
2021-11-02 10:17:29 +0530

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int n,x,y,z;
+	n=5;
+	x=n*(n+1)/2;
+	y=n*(n+1);
+	z= n*n;
+	printf("sum of first %d natural numbers is %d\n",n,x);
+	printf("sum of first %d even natural numbers is %d\n",n,y);
+	printf("sum of fisrt %d odd natural numbersis %d\n",n,z);
+	
+}
